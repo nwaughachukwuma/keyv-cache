f3aff92af843ea8203816ae1bf3eec071288ff68
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-12-11 07:35:18 +0400

    update tsd test

--- index.test-d.ts
+++ index.test-d.ts
@@ -3,3 +3,20 @@ import keyvCache, { CacheHandlers } from "./index";
 
 const cache = keyvCache();
 expectType<CacheHandlers<any> | null>(cache);
+
+if (cache) {
+  expectType<CacheHandlers<any>>(cache);
+  expectAssignable<CacheHandlers<any>>(cache);
+
+  // check the method signatures
+  expectType<CacheHandlers<any>["get"]>(cache.get);
+  expectType<CacheHandlers<any>["set"]>(cache.set);
+  expectType<CacheHandlers<any>["has"]>(cache.has);
+  expectType<CacheHandlers<any>["remove"]>(cache.remove);
+
+  // check the methods return type signatures
+  expectType<Promise<any>>(cache.get("key"));
+  expectType<Promise<void>>(cache.set("key", "value", 1000));
+  expectType<Promise<boolean>>(cache.has("key"));
+  expectType<Promise<boolean>>(cache.remove("key"));
+}
