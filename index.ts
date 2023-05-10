502496771d824d0344617675710a288efae07a4d
Chukwuma Nwaugha <nwaughac@gmail.com>
2023-05-09 20:42:46 -0700

    use in-memory cache if cache storage isn't supported

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 // Interfaces
 /** duration in milliseconds */
-type Milliseconds = number;
+export type Milliseconds = number;
 export interface CacheHandlers<T> {
   get(key: string): Promise<T | null>;
   set(key: string, value: T, ttl: Milliseconds): Promise<string>;
@@ -15,17 +15,17 @@ export interface CacheOptions {
   namespace?: string;
 }
 // Helpers
-function isValidURL(str: string) {
+export function isValidURL(str: string) {
   try {
     return !!new URL(str);
   } catch (e) {
     return false;
   }
 }
-function isBrowser() {
+export function isBrowser() {
   return typeof window !== "undefined";
 }
-function getCircularReplacer() {
+export function getCircularReplacer() {
   const seen = new WeakSet();
   return (_key: string, value: any) => {
     if (typeof value === "object" && value !== null) {
@@ -35,51 +35,51 @@ function getCircularReplacer() {
     return value;
   };
 }
-// Implementation
-class CacheWorker {
-  constructor(protected ns: string) {}
-  get caches() {
-    if (!isBrowser()) {
-      throw new ReferenceError("keyv-cache only works in the browser");
-    }
-    return window.caches;
-  }
-  makeKey(key: string) {
-    const decodedKey = decodeURIComponent(key);
-    return isValidURL(decodedKey) ? decodedKey : decodedKey + `:ns=${this.ns}`;
-  }
-  protected isValidKey(keyRes: Response) {
-    const now = Date.now();
-    const timestamp = keyRes.headers.get("timestamp") || now;
-    const ttl = keyRes.headers.get("ttl") || 0;
-    return +ttl + +timestamp >= now;
-  }
-  protected makeResponse(result: any, ttl: number) {
-    return new Response(JSON.stringify(result, getCircularReplacer()), {
-      headers: { timestamp: `${Date.now()}`, ttl: `${ttl}` },
-    });
-  }
+export function makeKey(key: string, namespace: string = "keyv-cache") {
+  const decodedKey = decodeURIComponent(key);
+  return isValidURL(decodedKey) ? decodedKey : decodedKey + `:ns=${namespace}`;
 }
-export default class KeyvCache<T>
-  extends CacheWorker
-  implements CacheHandlers<T>
-{
+export function isValidKey(keyRes: Response) {
+  const now = Date.now();
+  const timestamp = keyRes.headers.get("timestamp") || now;
+  const ttl = keyRes.headers.get("ttl") || 0;
+  return +ttl + +timestamp >= now;
+}
+
+export function makeResponse(result: any, ttl: number) {
+  return new Response(JSON.stringify(result, getCircularReplacer()), {
+    headers: { timestamp: `${Date.now()}`, ttl: `${ttl}` },
+  });
+}
+
+export default function KeyvCache<T>(options?: CacheOptions) {
+  return !isBrowser() || !window.caches || !window.caches.open
+    ? new InMemoryCache<T>()
+    : new BrowserCache<T>(options);
+}
+
+// Browser cache implementation
+class BrowserCache<T> implements CacheHandlers<T> {
+  private ns: string;
+  public type = "browser";
   constructor(opt?: CacheOptions) {
-    const ns = opt?.namespace || "keyv-cache";
-    super(ns);
-    this.ns = ns;
+    this.ns = opt?.namespace || "keyv-cache";
+  }
+
+  private get caches() {
+    return window.caches;
   }
 
   async set(key: string, value: T, ttl: Milliseconds) {
     const cache = await this.caches.open(this.ns);
-    await cache.put(this.makeKey(key), this.makeResponse(value, ttl));
+    await cache.put(makeKey(key, this.ns), makeResponse(value, ttl));
     return "OK";
   }
   async get(key: string) {
-    const formattedKey = this.makeKey(key);
+    const formattedKey = makeKey(key, this.ns);
     const cache = await this.caches.open(this.ns);
     const keyRes = await cache.match(formattedKey);
-    if (!keyRes?.ok || !this.isValidKey(keyRes)) {
+    if (!keyRes?.ok || !isValidKey(keyRes)) {
       await this.remove(formattedKey);
       return null;
     }
@@ -87,11 +87,12 @@ export default class KeyvCache<T>
     return keyRes.clone().json();
   }
   async has(key: string) {
-    return !!(await this.get(key));
+    const data = await this.get(key);
+    return data != null;
   }
   async remove(key: string) {
     const cache = await this.caches.open(this.ns);
-    return cache.delete(this.makeKey(key));
+    return cache.delete(makeKey(key, this.ns));
   }
   async removePattern(pattern: string) {
     const cache = await this.caches.open(this.ns);
@@ -105,5 +106,50 @@ export default class KeyvCache<T>
   }
   clear() {
     return this.caches.delete(this.ns);
+  }
+}
+
+// In memory cache implementation
+class InMemoryCache<T> implements CacheHandlers<T> {
+  private cache: Map<string, { value: T; expiry: number }> = new Map();
+  public type = "in-memory";
+
+  async set(key: string, value: T, ttl: Milliseconds): Promise<string> {
+    this.cache.set(key, { value, expiry: Date.now() + ttl });
+    return "OK";
+  }
+
+  async get(key: string): Promise<T | null> {
+    const data = this.cache.get(key);
+    if (data && data.expiry >= Date.now()) {
+      return data.value;
+    }
+    this.cache.delete(key);
+    return null;
+  }
+
+  async has(key: string): Promise<boolean> {
+    const data = await this.get(key);
+    return data != null;
+  }
+
+  async remove(key: string): Promise<boolean> {
+    return this.cache.delete(key);
+  }
+
+  async removePattern(pattern: string): Promise<Array<boolean>> {
+    const keysToDelete = Array.from(this.cache.keys()).filter((key) =>
+      key.includes(pattern)
+    );
+    return keysToDelete.map((key) => this.cache.delete(key));
+  }
+
+  async keys(): Promise<string[]> {
+    return Array.from(this.cache.keys());
+  }
+
+  async clear(): Promise<boolean> {
+    this.cache.clear();
+    return true;
   }
 }
